fecb744cdf22e02f90ffa80fdc01caa088317d02
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-02-14 12:48:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import axios from "axios";
+import { store } from "./store/index";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
